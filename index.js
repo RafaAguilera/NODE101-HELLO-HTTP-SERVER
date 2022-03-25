99434f8e3eb8e368a3b054083904276597357eae
Rafael <aguilera.morales.rafa@gmail.com>
2022-03-24 17:02:30 -0700

    Node101 hello http server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code here
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+res.writeHead(200, {'Content-Type': "text/plain"});
+res.end("Hello World");
+});
+
+server.listen(8080, '127.0.0.1');
+console.log("now listening to port 8080");
